fe2e33fe21ce361fc3f32b857f0580474baeb959
BenocxX <mathis.cote@outlook.com>
2025-07-31 13:25:22 -0400

    feat: add pagination controls

--- src/routes/savant/admin/user/+page.server.ts
+++ src/routes/savant/admin/user/+page.server.ts
@@ -1,36 +1,28 @@
 import { roleSchema } from '$lib/common/schemas/prisma-schema.js';
-import type { User } from '$lib/common/types/prisma-types.js';
 import { db } from '$lib/server/prisma';
-import { error } from '@sveltejs/kit';
 
 export const load = async ({ url }) => {
-  const result = roleSchema.safeParse(url.searchParams.get('role'));
-
-  if (!result.success) {
-    return error(404, 'Role not found');
-  }
-
-  let users: User[];
-
-  if (result.data === 'Student') {
-    users = await db.user.findMany({
-      where: {
-        role: 'Student',
-      },
-    });
-  } else if (result.data === 'Teacher') {
-    users = await db.user.findMany({
-      where: {
-        OR: [{ role: 'Teacher' }, { role: 'Admin' }],
-      },
-    });
-  } else {
-    users = await db.user.findMany({
-      where: {
-        role: 'Admin',
-      },
-    });
-  }
+  const users = await getUsersByRole(url.searchParams.get('role'));
 
   return { users };
 };
+
+async function getUsersByRole(role: string | null) {
+  if (role === null) {
+    return await db.user.findMany();
+  }
+
+  const result = roleSchema.safeParse(role);
+
+  if (!result.success) {
+    return [];
+  }
+
+  if (result.data === 'Student') {
+    return await db.user.findMany({ where: { role: 'Student' } });
+  } else if (result.data === 'Teacher') {
+    return await db.user.findMany({ where: { OR: [{ role: 'Teacher' }, { role: 'Admin' }] } });
+  }
+
+  return await db.user.findMany({ where: { role: 'Admin' } });
+}
