5f4d68933ea3eca008cdf6ea8dd32fc336ca125f
BenocxX <mathis.cote@outlook.com>
2025-08-14 11:33:44 -0400

    chore: fix cors problem

--- src/routes/api/course/+server.ts
+++ src/routes/api/course/+server.ts
@@ -4,5 +4,20 @@ import { json } from '@sveltejs/kit';
 export const GET = async (event) => {
   const courses = await db.course.findMany();
 
-  return json(courses);
+  return json(courses, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    },
+  });
+};
+
+export const OPTIONS = async () => {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Who can access
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Which methods are allowed
+      'Access-Control-Allow-Headers': 'Content-Type', // Which headers can be sent
+    },
+  });
 };
