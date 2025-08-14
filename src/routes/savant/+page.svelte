891d6feb54451057ede48ab89868f4c0327228e9
BenocxX <mathis.cote@outlook.com>
2025-08-14 18:09:46 -0400

    chore: remove useless stuff from home page

--- src/routes/savant/+page.svelte
+++ src/routes/savant/+page.svelte
@@ -1,14 +1,5 @@
-<script>
-  import Button from '$lib/client/components/ui/button/button.svelte';
+<script lang="ts">
+  import PageTitle from '$lib/client/components/structure/page-title.svelte';
 </script>
 
-<h2>Savant</h2>
-
-<a href="/savant/presentation">Presentation test</a>
-
-<Button>Primary</Button>
-<Button variant="secondary">Secondary</Button>
-<Button variant="destructive">Destructive</Button>
-<Button variant="outline">Outline</Button>
-<Button variant="ghost">Ghost</Button>
-<Button variant="link">Link</Button>
+<PageTitle title="Savant" />
