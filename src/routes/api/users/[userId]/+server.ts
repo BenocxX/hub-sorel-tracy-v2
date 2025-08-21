fec580f10682b29f1ca9fb0bb522ee526deafc0e
BenocxX <mathis.cote@outlook.com>
2025-08-21 17:45:20 -0400

    feat: add route to see individual user with their courses

---
+++ src/routes/api/users/[userId]/+server.ts
@@ -0,0 +1,37 @@
+import { db } from '$lib/server/prisma/index.js';
+import { json } from '@sveltejs/kit';
+
+export const GET = async ({ params }) => {
+  const user = await db.user.findFirst({
+    where: { role: 'Student', id: params.userId },
+    select: {
+      id: true,
+      username: true,
+      firstname: true,
+      lastname: true,
+      student: {
+        select: {
+          courses: {
+            select: {
+              id: true,
+              name: true,
+              icon: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const compactUser = user
+    ? {
+        id: user.id,
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        courses: user.student?.courses,
+      }
+    : null;
+
+  return json(compactUser);
+};
