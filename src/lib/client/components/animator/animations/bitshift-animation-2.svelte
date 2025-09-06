058d2ff832452ab622e7daa5d784a00a654399a3
BenocxX <mathis.cote@outlook.com>
2025-09-06 17:19:48 -0400

    fix: remove d3 from project, was blocking build for weird reasons

--- src/lib/client/components/animator/animations/bitshift-animation-2.svelte
+++ src/lib/client/components/animator/animations/bitshift-animation-2.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-  import { all, TweenStepper } from '../tween-stepper.svelte';
+  import { TweenStepper } from '../tween-stepper.svelte';
   import { tweener } from '../tweener.svelte';
-  import { interpolateLab } from 'd3-interpolate';
   import TweenStepperControls from '../tween-stepper-controls.svelte';
 
   const stepper = new TweenStepper();
 
   const position = tweener({ left: 25, top: 50, right: 0, bottom: 0 }, { duration: 500 });
-  const color = tweener('#FFF', { interpolate: interpolateLab });
 
-  stepper.addStep(() => all(position.to({ left: 25, top: 50 }), color.to('#FFF')));
+  stepper.addStep(() => position.to({ left: 25, top: 50 }));
   stepper.addStep(() => position.to({ left: 75, top: 50 }));
-  stepper.addStep(() => all(position.to({ left: 75, top: 25 }), color.to('#FF0000')));
+  stepper.addStep(() => position.to({ left: 75, top: 25 }));
   stepper.addStep(() => position.to({ left: 75, top: 75 }));
   stepper.addStep(() => position.to({ left: 75, top: 50 }));
 </script>
@@ -20,7 +18,7 @@
   <div
     class="absolute h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full"
     style="left: {position.current.left}%; top: {position.current.top}%; right: {position.current
-      .right}%; bottom: {position.current.bottom}%; background-color: {color.current};"
+      .right}%; bottom: {position.current.bottom}%;"
   ></div>
 </div>
 <TweenStepperControls {stepper} />
