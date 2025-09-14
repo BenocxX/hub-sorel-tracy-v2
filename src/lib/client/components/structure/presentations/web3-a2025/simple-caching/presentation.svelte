d0ba5075cbea780752fea9b10131284cc8d812c8
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-09-14 11:54:56 -0400

    feat: add presentation for caching

--- src/lib/client/components/structure/presentations/web3-a2025/simple-caching/presentation.svelte
+++ src/lib/client/components/structure/presentations/web3-a2025/simple-caching/presentation.svelte
@@ -2,10 +2,18 @@
   import PresentationRoot from '$lib/client/components/revealjs/custom/presentation-root.svelte';
   import { currentPresentation } from '$lib/client/components/revealjs/store.svelte';
   import { initializeSlideLinks } from '$lib/client/components/revealjs/custom/utils.svelte';
-  import Caching from './caching.svelte';
+  import Intro from './intro.svelte';
+  import Expiry from './expiry.svelte';
+  import Write from './write.svelte';
+  import Read from './read.svelte';
+  import Usage from './usage.svelte';
 
   const sections = {
-    caching: 'Mise en cache des résultats',
+    intro: 'Introduction',
+    expiry: 'Expiration du cache',
+    write: 'Mise en cache des résultats',
+    read: 'Lecture du cache',
+    usage: "Utilisation d'une mise en cache",
   };
 
   currentPresentation.title = "Utilisation d'une mise en cache";
@@ -14,5 +22,9 @@
 </script>
 
 <PresentationRoot>
-  <Caching section={sections.caching} />
+  <Intro section={sections.intro} />
+  <Expiry section={sections.expiry} />
+  <Write section={sections.write} />
+  <Read section={sections.read} />
+  <Usage section={sections.usage} />
 </PresentationRoot>
---
+++ src/lib/client/components/structure/presentations/web3-a2025/simple-caching/write.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import {
+    BasicSlide,
+    InlineCodeBlock,
+    CodeBlock,
+    setSlideSection,
+    registerInTOC,
+    type SlideSectionProps,
+  } from '$lib/client/components/revealjs/custom';
+
+  const { section }: SlideSectionProps = $props();
+
+  setSlideSection(section);
+  registerInTOC({ page: 2 });
+
+  const writeCodes = [
+    `export function writeCache(key, value, timeToLiveInMs) {
+
+}`,
+
+    `export function writeCache(key, value, timeToLiveInMs) {
+  const now = new Date();
+}`,
+
+    `export function writeCache(key, value, timeToLiveInMs) {
+  const now = new Date();
+
+  const item = {
+    value: value,
+  };
+}`,
+
+    `export function writeCache(key, value, timeToLiveInMs) {
+  const now = new Date();
+
+  const item = {
+    value: value,
+    expiry: now.getTime() + timeToLiveInMs,
+  };
+}`,
+
+    `export function writeCache(key, value, timeToLiveInMs) {
+  const now = new Date();
+
+  const item = {
+    value: value,
+    expiry: now.getTime() + timeToLiveInMs,
+  };
+
+  localStorage.setItem(key, JSON.stringify(item));
+}`,
+  ];
+</script>
+
+<BasicSlide>
+  <p>
+    Nous allons voir ensemble comment écrire une petite fonction réutilisable de mise en cache en
+    utilisant le <InlineCodeBlock>LocalStorage</InlineCodeBlock>.
+  </p>
+  <p>
+    Notez qu'il est essentiel d'utiliser le <InlineCodeBlock>JSON.stringify</InlineCodeBlock> pour stocker
+    des objets dans le <InlineCodeBlock>LocalStorage</InlineCodeBlock>, car il ne supporte que les
+    chaînes de caractères.
+  </p>
+</BasicSlide>
+{#each writeCodes as code, index (index)}
+  <BasicSlide>
+    <CodeBlock autoAnimateId="write-code" language="JS" {code} />
+  </BasicSlide>
+{/each}
