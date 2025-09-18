21e55dfc571dd0ccd4d79706a83248117239f975
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-09-18 11:38:29 -0400

    feat: start concepts-objets-avances presentation

---
+++ src/lib/client/components/structure/presentations/poo2-a2025/concepts-objets-avances/presentation.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import PresentationRoot from '$lib/client/components/revealjs/custom/presentation-root.svelte';
+  import { currentPresentation } from '$lib/client/components/revealjs/store.svelte';
+  import { initializeSlideLinks } from '$lib/client/components/revealjs/custom/utils.svelte';
+  import ConceptsBases from './concepts-bases.svelte';
+  import RappelHeritage from './rappel-heritage.svelte';
+  import Abstraction from './abstraction.svelte';
+  import Finalisation from './finalisation.svelte';
+  import RappelRedefinition from './rappel-redefinition.svelte';
+  import ReglesHeritages from './regles-heritages.svelte';
+
+  const sections = {
+    conceptsBases: 'Concepts de bases et vocabulaires',
+    rappelHeritage: "Rappels sur l'héritage",
+    rappelRedefinition: 'Rappels sur la redéfinition',
+    reglesHeritages: "Règles sur l'héritages",
+    finalisation: 'Finalisation',
+    abstraction: 'Abstraction',
+  };
+
+  currentPresentation.title = 'Concepts objets avancés';
+
+  initializeSlideLinks();
+</script>
+
+<PresentationRoot>
+  <ConceptsBases section={sections.conceptsBases} />
+  <RappelHeritage section={sections.rappelHeritage} />
+  <RappelRedefinition section={sections.rappelRedefinition} />
+  <ReglesHeritages section={sections.reglesHeritages} />
+  <Finalisation section={sections.finalisation} />
+  <Abstraction section={sections.abstraction} />
+</PresentationRoot>
