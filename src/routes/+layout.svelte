f827a4adb7ec8062c91f03ae89ab7c56f062164e
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2025-09-29 14:00:23 -0400

    feat: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,15 @@
   import '$lib/client/highlightjs-dark.css';
   import '$lib/client/highlightjs-light.css';
   import { Toaster } from '$lib/client/components/ui/sonner/index.js';
+  import HubLogoFavIcon from '$lib/client/assets/images/hub-logo-no-text-removed-bg.ico';
 
   const { children } = $props();
 </script>
+
+<svelte:head>
+  <link rel="icon" href={HubLogoFavIcon} />
+  <title>Hub du Cégep de Sorel-Tracy</title>
+</svelte:head>
 
 <Toaster />
 {@render children()}
