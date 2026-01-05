d1115baa08568caae593a8ce23d357cd1083e156
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-01-04 20:26:19 -0500

    feat: add shadcn-svelte input-group component

---
+++ src/lib/client/components/ui/input-group/input-group-text.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { cn, type WithElementRef } from '$lib/client/utils';
+  import type { HTMLAttributes } from 'svelte/elements';
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
+</script>
+
+<span
+  bind:this={ref}
+  class={cn(
+    "flex items-center gap-2 text-sm text-foreground-discreet [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none",
+    className
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</span>
