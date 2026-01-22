eb2c4285b08868c9dd81e5ff249cd6137393e35e
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-01-21 22:58:56 -0500

    feat: add slides to intro-et-histoire presentation

--- src/lib/client/components/structure/presentations/procedural-h2026/intro-et-histoire/presentation.svelte
+++ src/lib/client/components/structure/presentations/procedural-h2026/intro-et-histoire/presentation.svelte
@@ -4,10 +4,14 @@
   import { initializeSlideLinks } from '$lib/client/components/revealjs/custom/utils.svelte';
   import Historique from './historique.svelte';
   import NiveauLangages from './niveau-langages.svelte';
+  import ResoudreProbleme from './resoudre-probleme.svelte';
+  import ParadigmeProgrammation from './paradigme-programmation.svelte';
 
   const sections = {
     historique: "Introduction et l'historique du langage C",
     niveauLangages: 'Niveau des langages de programmation',
+    resoudreProbleme: 'Comment résoudre un problème',
+    paradigmeProgrammation: "Qu'est-ce qu'un paradigme de programmation",
   };
 
   currentPresentation.title = 'Introduction et histoire du langage C';
@@ -18,4 +22,6 @@
 <PresentationRoot>
   <Historique section={sections.historique} />
   <NiveauLangages section={sections.niveauLangages} />
+  <ResoudreProbleme section={sections.resoudreProbleme} />
+  <ParadigmeProgrammation section={sections.paradigmeProgrammation} />
 </PresentationRoot>
