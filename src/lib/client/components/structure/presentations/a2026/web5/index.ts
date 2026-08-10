e905932379fa0462f4c49ad3a88a07adf4347622
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-08-10 13:41:06 -0400

    feat: add presentation historique-frameworks

--- src/lib/client/components/structure/presentations/a2026/web5/index.ts
+++ src/lib/client/components/structure/presentations/a2026/web5/index.ts
@@ -1,7 +1,9 @@
 import IntroFrameworks from './intro-frameworks.svx';
+import HistoriqueFrameworks from './historique-frameworks.svx';
 
 export function makeWeb5PresentationComponents() {
   return [
     { id: 'web5-a2026-intro-frameworks', component: IntroFrameworks },
+    { id: 'web5-a2026-historique-frameworks', component: HistoriqueFrameworks },
   ];
 }
