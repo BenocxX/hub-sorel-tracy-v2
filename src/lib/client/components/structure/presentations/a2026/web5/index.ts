ecaae8dcdac819c4e38de0574af1f15764bb020b
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-08-11 11:27:46 -0400

    feat: start presentation outils-developpement

--- src/lib/client/components/structure/presentations/a2026/web5/index.ts
+++ src/lib/client/components/structure/presentations/a2026/web5/index.ts
@@ -1,9 +1,11 @@
 import IntroFrameworks from './intro-frameworks.svx';
 import HistoriqueFrameworks from './historique-frameworks.svx';
+import OutilsDeveloppement from './outils-developpement.svx';
 
 export function makeWeb5PresentationComponents() {
   return [
     { id: 'web5-a2026-intro-frameworks', component: IntroFrameworks },
     { id: 'web5-a2026-historique-frameworks', component: HistoriqueFrameworks },
+    { id: 'web5-a2026-outils-developpement', component: OutilsDeveloppement },
   ];
 }
