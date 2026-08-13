db5727ba5d75e62cef6ff9c80c295721eb231839
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-08-13 16:51:40 -0400

    feat: add presentation structure-et-espacement

--- src/lib/client/components/structure/presentations/a2026/conception-interfaces/index.ts
+++ src/lib/client/components/structure/presentations/a2026/conception-interfaces/index.ts
@@ -1,7 +1,9 @@
 import PlanificationDesign from './planification-design.svx';
+import StructureEtEspacement from './structure-et-espacement.svx';
 
 export function makeConceptionInterfacesPresentationComponents() {
   return [
     { id: 'conception-interfaces-a2026-planification-design', component: PlanificationDesign },
+    { id: 'conception-interfaces-a2026-structure-et-espacement', component: StructureEtEspacement },
   ];
 }
