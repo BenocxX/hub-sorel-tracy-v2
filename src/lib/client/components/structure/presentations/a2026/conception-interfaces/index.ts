f1f62a17bfe03353c6aa88a64a3ca54f2a4eb5e3
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-08-23 12:43:14 -0400

    feat: start presentation importance-hierarchie

--- src/lib/client/components/structure/presentations/a2026/conception-interfaces/index.ts
+++ src/lib/client/components/structure/presentations/a2026/conception-interfaces/index.ts
@@ -1,9 +1,11 @@
 import PlanificationDesign from './planification-design.svx';
 import StructureEtEspacement from './structure-et-espacement.svx';
+import ImportanceHierarchie from './importance-hierarchie.svx';
 
 export function makeConceptionInterfacesPresentationComponents() {
   return [
     { id: 'conception-interfaces-a2026-planification-design', component: PlanificationDesign },
     { id: 'conception-interfaces-a2026-structure-et-espacement', component: StructureEtEspacement },
+    { id: 'conception-interfaces-a2026-importance-hierarchie', component: ImportanceHierarchie },
   ];
 }
