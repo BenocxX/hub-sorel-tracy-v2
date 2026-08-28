c54bb02f336205e512be913c5c5d8828435c16c3
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-08-28 14:02:02 -0400

    feat: start presentation intro-composants-react

--- src/lib/client/components/structure/presentations/a2026/web5/index.ts
+++ src/lib/client/components/structure/presentations/a2026/web5/index.ts
@@ -1,11 +1,13 @@
 import IntroFrameworks from './intro-frameworks.svx';
 import HistoriqueFrameworks from './historique-frameworks.svx';
 import OutilsDeveloppement from './outils-developpement.svx';
+import IntroComposantsReact from './intro-composants-react.svx';
 
 export function makeWeb5PresentationComponents() {
   return [
     { id: 'web5-a2026-intro-frameworks', component: IntroFrameworks },
     { id: 'web5-a2026-historique-frameworks', component: HistoriqueFrameworks },
     { id: 'web5-a2026-outils-developpement', component: OutilsDeveloppement },
+    { id: 'web5-a2026-intro-composants-react', component: IntroComposantsReact },
   ];
 }
