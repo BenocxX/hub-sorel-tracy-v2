2c5bb32427240d59abf98d470f6d346416e1c7f4
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2026-09-01 22:07:33 -0400

    fix: bad url

--- src/lib/client/components/structure/presentations/a2025/web1/intro-html/intro.svelte
+++ src/lib/client/components/structure/presentations/a2025/web1/intro-html/intro.svelte
@@ -26,7 +26,7 @@
     <Italic>youtube.com, facebook.com, google.com</Italic>, etc.) suivi d'un chemin vers la page
     ciblée (index.html) tout en étant préfixé par le protocole HTTP ou HTTPS. Par exemple, <InlineCodeBlock
       allowCopy
-      >https://monsite.com/index.html
+      >https://hubcst.ca/
     </InlineCodeBlock>.
   </p>
   <p>Tous les sites accessibles publiquement constituent le World Wide Web.</p>
